6d05e5b828d688aab55746b7a259d0c9c17f17e2
Purvansh Parmar <purvanshparmar15@gmail.com>
2023-02-25 14:15:56 +0530

    This is first tag

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 Flask demo apis
 
 
-1. python app.py
+1. git clone <url>
+2. python -m virtualenv venvpurvasnh
+3. python -m pip install Flask
+5. python app.py
---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask ,render_template,request
+import json
+app = Flask(__name__)
+
+listdata = [
+{
+    "name":"purvansh",
+    "age":22,
+    "id":1,
+    "h":True
+    },
+    {
+    "name":"ram",
+    "age":21,
+     "h":True,
+    "id":2
+    }
+]
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',listdata = listdata)
+
+@app.route('/data')
+def data():
+    return json.dumps(listdata)
+
+@app.route('/data/<int:id>')
+def data_for_id(id):
+    for i in listdata:
+        if i.get("id") == id:
+            return i
+    # return json.dumps(listdata)
+
+@app.route('/dataadd',methods=['POST'])
+def data_for_name():
+    if request.method == 'POST':
+        print(request.data)
+        data = json.loads(request.data)
+        name = data.get("name")
+        age = data.get("age")
+        h = data.get("h")
+        nedata = {"name":name,"age":age,"h":h,"id":len(listdata)}
+        listdata.append(nedata)
+        return json.dumps(nedata)
+        
+
+if __name__ =="__main__":
+    app.run(debug=True)
+
+
