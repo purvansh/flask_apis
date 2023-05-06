32bdd75540345ba58556cb5de308212890267e27
Purvansh Parmar <purvanshparmar15@gmail.com>
2023-05-06 12:07:54 +0530

    updated the demo code

--- app.py
+++ app.py
@@ -1,53 +1,43 @@
-from flask import Flask ,render_template,request
+from flask import Flask ,render_template,request,redirect
 import json
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
 app = Flask(__name__)
 
-listdata = [
-{
-    "name":"purvansh",
-    "age":22,
-    "id":1,
-    "h":True
-    },
-    {
-    "name":"ram",
-    "age":21,
-     "h":True,
-    "id":2
-    }
-]
+app.config['MYSQL_HOST'] = 'sql12.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql12616339'
+app.config['MYSQL_PASSWORD'] = '28ZLshkDpQ'
+app.config['MYSQL_DB'] = 'sql12616339'
 
+mysql = MySQL(app)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html',listdata = listdata)
+    return render_template('register.html')
 
 @app.route('/data')
 def data():
-    return json.dumps(listdata)
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM accounts')
+    return render_template('index.html',listdata =cursor.fetchall  ())    
 
-@app.route('/data/<int:id>')
-def data_for_id(id):
-    for i in listdata:
-        if i.get("id") == id:
-            return i
-    # return json.dumps(listdata)
 
-@app.route('/dataadd',methods=['POST'])
+@app.route('/dataadd',methods=['POST','GET'])
 def data_for_name():
     if request.method == 'POST':
-        print(request.data)
-        data = json.loads(request.data)
-        name = data.get("name")
-        age = data.get("age")
-        h = data.get("h")
-        nedata = {"name":name,"age":age,"h":h,"id":len(listdata)}
-        listdata.append(nedata)
-        return json.dumps(nedata)
+        print(request)
+        print(request.form)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        email = request.form['email']
+        password = request.form['password']
+        cursor.execute("insert into accounts(email,password) values('"+email+"','"+password+"') ")
+        mysql.connection.commit()
+        return redirect('/data')
         
+@app.route('/updatedata',methods=['POST','GET'])
+def updatedata():
+    return render_template('updatepassword.html')
 
 if __name__ =="__main__":
     app.run(debug=True,host="0.0.0.0",port = 80)
-
-
